f74469849f4a81007a7f9c6c8832653b33e8bb7d
Rajdeep Das <58761773+aSpanefRost@users.noreply.github.com>
2020-11-22 19:40:27 +0530

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+#include<climits>
+#include<vector>
+#include<string>
+#include<cstring>
+#include<set>
+#include<algorithm>
+using namespace std;
+ 
+#define fo(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define lli long long int
+#define mem(a,n) memset(a,n,sizeof(a))
+#define de(x) cout<<"*"<<x<<"*"<<endl
+const int mod=1000000007;
+ 
+ 
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+  
+ 
+    
+
+   
+  return 0;
+}
