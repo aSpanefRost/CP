31abc2d1c45591885b68bd48edab80360cd75c46
Rajdeep Das <58761773+aSpanefRost@users.noreply.github.com>
2021-01-18 21:32:15 +0530

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,51 @@
+void bfs(vector<int> ar[],int N) 
+{
+   int vis[N+1];
+   for(int i=0;i<=N;i++) vis[i]=0;
+   queue<int> q;
+   int c=1;
+   q.push(c);
+   //cout<<c;
+   vis[c]=1;
+   while(!q.empty())
+   {
+   	  int v=q.front();
+   	  q.pop();
+   	  cout<<v<<"->";
+   	  for(int x:ar[v])
+   	  {
+   	  	if(vis[x]==0)
+   	  	{
+   	  		vis[x]=1;
+   	  		q.push(x);
+   	  	}
+   	  }
+   }	
+
+}
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int N,M;
+    cin>>N>>M;
+    vector<int> ar[N+1];
+    int a,b;
+    for(int i=1;i<=M;i++)
+    {  
+    	cin>>a>>b;
+    	a++;
+    	b++;
+       ar[a].push_back(b);
+       ar[b].push_back(a);
+    }
+    //int vis[N+1];
+    //for(int i=1;i<=N;i++) vis[i]=0;
+    bfs(ar,N);
+    return 0;
+}
