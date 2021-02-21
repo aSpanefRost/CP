86e0204496f20a936b342290801707a45fb09f1f
Rajdeep Das <58761773+aSpanefRost@users.noreply.github.com>
2021-02-21 21:53:51 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -17,17 +17,29 @@ using namespace std;
 const int mod=1000000007;
  
  
- 
+ void solve()
+ {
+  
+ }
  
 int main()
 {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
  
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
- 
-  
+    
+    int t;
+    t=1;
+    //cin>>t;
+     while(t--)
+     {
+      solve();
+     }
  
     
 
