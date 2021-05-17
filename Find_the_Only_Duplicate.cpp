90661cdc2eeda5b18355031009c2010e32ad0f6a
Rajdeep Das <58761773+aSpanefRost@users.noreply.github.com>
2021-05-17 20:43:19 +0530

    Update Find_the_Only_Duplicate.cpp

--- Find_the_Only_Duplicate.cpp
+++ Find_the_Only_Duplicate.cpp
@@ -3,6 +3,9 @@
 //As there is a circle then , we can use the tortoise method
 //Tortoise method will help use finding the duplicate
 
+//Tortoise mehod ::
+//The idea behind the algorithm is that, if you have two pointers in a linked list, one moving twice as fast (the hare) than the other (the tortoise), 
+//then if they intersect, there is a cycle in the linked list. If they don't intersect, then there is no cycle
 int findDuplicate(vector<int>& nums) 
     {
        int fast=nums[0];
